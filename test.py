04496ed00be5eb34fa7ae6752986fee6e700835d
Danya <dan.shell@bk.ru>
2023-10-01 22:22:29 +0300

    sort arrays with comments

--- test.py
+++ test.py
@@ -1,83 +1,85 @@
+
 import random
 
-def sort_massives(main_mas):
 
-    for element in range(0,len(main_mas),2):
-        main_mas[element]=merge_sort(main_mas[element])
-    for element in range(1,len(main_mas),2):
-        main_mas[element]=merge_sort(main_mas[element])
-        main_mas[element]= main_mas[element][::-1]
+def create_arrays(n):
+    # this  main array will contain little arrays
+    main_arr = []
 
-    return main_mas
+    # generate lens of little arrays
+    numbers = list(range(1, n+1))
+    random.shuffle(numbers)
 
-def merge_sort (arr):
-    # базовый случай: если массив состоит из одного элемента или пуст, он уже отсортирован
+    # generate n little arrays
+    for i in range(n):
+        m = []
+        # choose len of little array
+        little_arr_len = numbers[i]
+
+        # fill little array
+        for j in range(1, little_arr_len+1):
+            a = random.uniform(1, 10)
+            m.append(a)
+        main_arr.append(m)
+    main_arr = sort_arrays(main_arr)
+    return main_arr
+
+
+# funtion to sort arrays which are included in the main array
+def sort_arrays(main_arr):
+    # sort even elements
+    for element in range(0, len(main_arr), 2):
+        main_arr[element] = merge_sort(main_arr[element])
+    # sort odd elements
+    for element in range(1, len(main_arr), 2):
+        main_arr[element] = merge_sort(main_arr[element])
+        # turn over array
+        main_arr[element] = main_arr[element][::-1]
+
+    return main_arr
+
+
+def merge_sort(arr):
+    # if arr consists of 1 element, it means it has already sorted
+
     if len(arr) <= 1:
         return arr
 
-    # разделяем массив пополам
+    # devide array on halfs
     mid = len(arr) // 2
     left_half = arr[:mid]
     right_half = arr[mid:]
 
-    # рекурсивно сортируем обе половины
+    # sort halfs with  help of recursion
     left_sorted = merge_sort(left_half)
     right_sorted = merge_sort(right_half)
 
-    # сливаем отсортированные половины
+    # merge sorted halfs
     return merge_arrays(left_sorted, right_sorted)
-    
+
 
 def merge_arrays(left, right):
-    merge_array=[]
-    l=len(left)
-    r=len(right)
-    i=0
-    j=0
-    while i<l and j <r:
-        if left[i]<=right[j]:
+    merge_array = []
+    # find left and right borders
+    l = len(left)
+    r = len(right)
+    i = 0
+    j = 0
+    # merge to little elements go firstly
+    while i < l and j < r:
+        if left[i] <= right[j]:
             merge_array.append(left[i])
-            i+=1
+            i += 1
         else:
             merge_array.append(right[j])
-            j+=1
-    while i <l:
+            j += 1
+    # check if arrays haven't equal length
+    while i < l:
         merge_array.append(left[i])
-        i+=1
-    while j <r:
+        i += 1
+    while j < r:
         merge_array.append(right[j])
-        j+=1
+        j += 1
     return merge_array
 
-def create_massives(n):
-    # this massive will contain little massives
-    main_mas=[]
-    
-    # generate lens of little massives  
-    numbers = list(range(1, n+1))
-    random.shuffle(numbers)
-
-    # generate n little massives
-    for i in range(n):
-        m=[]
-        # choose len of little massive
-        little_mas_len=numbers[i]
-
-
-        # fill little massive
-        for j in range(1,little_mas_len+1):
-            a=random.uniform(1,10)
-            m.append(a)
-        main_mas.append(m)
-    main_mas=sort_massives(main_mas)    
-    return main_mas
-
-print(create_massives(5))
-
-
-
-
-
-
-print(merge_arrays([3,9],[2,3,6]))
 
