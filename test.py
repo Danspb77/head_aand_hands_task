fda2ef0d6037a58c85eaa2b3980f12fbc2bb4e8f
Danya <dan.shell@bk.ru>
2023-10-01 17:07:29 +0300

    sort arrays without commnts

--- test.py
+++ test.py
@@ -1,5 +1,54 @@
 import random
 
+def sort_massives(main_mas):
+
+    for element in range(0,len(main_mas),2):
+        main_mas[element]=merge_sort(main_mas[element])
+    for element in range(1,len(main_mas),2):
+        main_mas[element]=merge_sort(main_mas[element])
+        main_mas[element]= main_mas[element][::-1]
+
+    return main_mas
+
+def merge_sort (arr):
+    # базовый случай: если массив состоит из одного элемента или пуст, он уже отсортирован
+    if len(arr) <= 1:
+        return arr
+
+    # разделяем массив пополам
+    mid = len(arr) // 2
+    left_half = arr[:mid]
+    right_half = arr[mid:]
+
+    # рекурсивно сортируем обе половины
+    left_sorted = merge_sort(left_half)
+    right_sorted = merge_sort(right_half)
+
+    # сливаем отсортированные половины
+    return merge_arrays(left_sorted, right_sorted)
+    
+
+def merge_arrays(left, right):
+    merge_array=[]
+    l=len(left)
+    r=len(right)
+    i=0
+    j=0
+    while i<l and j <r:
+        if left[i]<=right[j]:
+            merge_array.append(left[i])
+            i+=1
+        else:
+            merge_array.append(right[j])
+            j+=1
+    while i <l:
+        merge_array.append(left[i])
+        i+=1
+    while j <r:
+        merge_array.append(right[j])
+        j+=1
+    return merge_array
+
 def create_massives(n):
     # this massive will contain little massives
     main_mas=[]
@@ -20,9 +69,15 @@ def create_massives(n):
             a=random.uniform(1,10)
             m.append(a)
         main_mas.append(m)
-        
-    return main_mas, numbers
+    main_mas=sort_massives(main_mas)    
+    return main_mas
+
+print(create_massives(5))
 
 
-print(create_massives(1))
-            
+
+
+
+
+print(merge_arrays([3,9],[2,3,6]))
+
